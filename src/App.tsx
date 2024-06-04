5fa890cfbd50c5c64f261efad4bd784a2f6e7819
Aakash Saxena <aakashspecs@gmail.com>
2024-06-04 11:12:17 +0530

    App component responsive

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
 
   return (
     <div className="relative items-center justify-center h-screen bg-black sm:flex">
-      <div className="absolute bottom-0 w-full sm:h-min sm:w-[600px] sm:relative bg-darkGrey rounded-t-xl sm:rounded-xl ">
+      <div className="absolute bottom-0 w-full sm:h-min sm:w-[600px] sm:relative bg-darkGrey rounded-t-xl sm:rounded-xl max-h-[calc(100vh-20%)] overflow-y-scroll sm:overflow-auto  ">
         <div className="absolute flex items-center justify-center text-center bg-black rounded-full text-crossGrey top-4 right-4 w-9 h-9 ">
           <RxCross1 />
         </div>
